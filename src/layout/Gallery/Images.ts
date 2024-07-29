b995923eb94cdc32734a1486ffd33a3b6f67c8a3
taeyun1215 <taeyun1215@naver.com>
2024-07-29 21:41:37 +0900

    ㅅㄷㄴㅅ

--- src/layout/Gallery/Images.ts
+++ src/layout/Gallery/Images.ts
@@ -2,7 +2,7 @@ import image01 from '@/assets/images/01.jpg'
 import image02 from '@/assets/images/02.jpg'
 import image03 from '@/assets/images/03.jpg'
 import image04 from '@/assets/images/04.jpg'
-import image05 from '@/assets/images/05.jpg'
+// import image05 from '@/assets/images/05.jpg'
 import image06 from '@/assets/images/06.jpg'
 import image07 from '@/assets/images/07.jpg'
 import image08 from '@/assets/images/08.jpg'
@@ -30,9 +30,6 @@ const images = [   /* 이미지 경로를 리스트로 저장 */
   },{
     alt: 'image04',
     source: image04,
-  },{
-    alt: 'image05',
-    source: image05,
   },{
     alt: 'image06',
     source: image06,
